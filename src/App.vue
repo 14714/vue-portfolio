9608ac9dfed4c48827fb2a4deb1ea34df84f0836
Matias Castillo <rivera.matias@hotmail.com>
2022-03-19 16:15:22 -0300

    Integracion de Tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,10 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You did'n it!" />
     </div>
   </header>
-
+  <h1 class="text-3xl font-bold underline">Hola Mundillo</h1>
   <main>
     <TheWelcome />
   </main>
