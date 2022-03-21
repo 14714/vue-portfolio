0bb95f34956afead28942f846f8c38ad36f28767
Matias Castillo <rivera.matias@hotmail.com>
2022-03-21 09:21:46 -0300

    Header, Hero and Macbook components listos.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import Header from './components/Header.vue'
 import Hero from './components/Hero.vue'
+import Macbook from './components/Macbook.vue'
+import Experience from './components/Experience.vue'
 </script>
 
 <template>
@@ -8,6 +10,40 @@ import Hero from './components/Hero.vue'
 		<Header/>
 	</header>
 	<Hero/>
+	<div class="mx-12">
+		<Macbook>
+			<div class="container ">
+				<pre><code>
+					<span class="text-[#7000FF]">.SobreMi</span> {
+						<span class="text-[#0EE6B7]">Profesion:</span> Fullstack Developer;       
+						<span class="text-[#0EE6B7]">Lenguajes:</span> PHP, JS, SQL, CSS; 
+					}
+					<span class="text-[#7000FF]">.Educacion</span> {
+						<span class="text-[#0EE6B7]">DuocUC:</span> Analista Desarrollador       
+						de Aplicaciones Software; 
+					}</code></pre>
+			</div>
+		</Macbook>
+		<h3 class="text-lg font-bold py-2">Experiencia</h3>
+		<div class="flex space-x-8	">
+			<Macbook>
+				<p>2022 - <span class="text-lg font-bold py-2">SMEC</span></p>
+				<img src="./assets/smec.png" alt="SMEC" class="mx-auto w-3/4">
+			</Macbook>
+			<Macbook>
+				<p>2020 - 2021 <span class="text-lg font-bold py-2">Clinica ISV</span></p>
+				<img src="./assets/clinicaisv.png" alt="SMEC" class="mx-auto w-3/4">
+			</Macbook>
+			<Macbook>
+				<p>2018 - 2019 <span class="text-lg font-bold py-2">Gemma Graphic</span></p>
+				<img src="./assets/gemg.png" alt="SMEC" class="mx-auto w-3/4">
+			</Macbook>
+		</div>
+		<h3 class="text-lg font-bold py-2">Ultimos Trabajos</h3>
+		<Experience>
+
+		</Experience>
+	</div>
 </template>
 
 <style>
---
+++ src/components/Macbook.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="bg-[#171A1B] rounded-md border-solid border-2 border-[#4A4A4A] w-full h-min pb-14">
+        <div class="flex justify-between p-3">
+            <div class="flex space-x-3">
+                <a href="#" class="nav-dot block w-4 h-4 rounded-full bg-[#FFBD2E]"></a>
+                <a href="#" class="nav-dot block w-4 h-4 rounded-full bg-[#FF5F56]"></a>
+                <a href="#" class="nav-dot block w-4 h-4 rounded-full bg-[#27C93F]"></a>
+            </div>
+            <div class="bg-[#202226] rounded-md px-24">
+                <h5>Matias.Castillo.com</h5>
+            </div>
+            <div></div>
+        </div>
+        <slot></slot>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
